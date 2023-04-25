bd5c184888e294e68affd5310f89b350d098222c
erikbasic <basicerik@gmail.com>
2023-04-25 14:20:02 +0200

    Dodan pokemon cell

---
+++ PokemonApp/PokemonCell.swift
@@ -0,0 +1,44 @@
+//
+//  PokemonCell.swift
+//  PokemonApp
+//
+//  Created by Erik Basic on 25.04.2023..
+//
+
+import UIKit
+
+// MARK: - Pokemon Cell Delegate Protocol
+protocol PokemonCellDelegate{
+  func addTapped(_ cell: PokemonCell)
+}
+
+// MARK: - Pokemon Cell
+
+class PokemonCell: UITableViewCell{
+  
+//  MARK: - Class constants
+  static let identifier = "PokemonCell"
+  
+//  MARK: - IBOutlets
+  
+  @IBOutlet weak var pokemonLabel: UITextField!
+  @IBOutlet weak var addButton: UIButton!
+  
+//  MARK: - Variables And Properties
+  
+  var delegate: PokemonCellDelegate?
+  
+// MARK: - IBActions
+  
+  
+  @IBAction func addTapped(_ sender: AnyObject) {
+    delegate?.addTapped(self)
+  }
+  
+//  MARK: - Internal Methods
+  
+  func configure(pokemon: Pokemon){
+    pokemonLabel.text = pokemon.name
+  }
+  
+}
