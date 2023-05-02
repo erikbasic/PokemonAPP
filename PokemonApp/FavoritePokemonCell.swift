a3190fec702d927b6fcd47cd6db2ab3572d7e36e
erikbasic <basicerik@gmail.com>
2023-05-02 10:36:40 +0200

    Implements favorite pokemon cell

--- PokemonApp/FavoritePokemonCell.swift
+++ PokemonApp/FavoritePokemonCell.swift
@@ -4,10 +4,20 @@
 //
 //  Created by Erik Basic on 02.05.2023..
 //
+// It defines favorite pokemon cell
 
 import UIKit
 
-
-
 // MARK: - Favorite Pokemon Cell
 
+class FavoritePokemonCell: UITableViewCell{
+//  MARK: - Class Constants
+  static let identifier = "FavoritePokemonCell"
+  
+//  MARK: - IBOutlets
+  @IBOutlet weak var favoritePokemonLabel: UILabel!
+  
+  func configure(favoritePokemon: Pokemon){
+    favoritePokemonLabel.text = favoritePokemon.name
+  }
+}
