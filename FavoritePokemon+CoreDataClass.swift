fce2817d4ee365af64187cccd8735d32fedfd919
erikbasic <basicerik@gmail.com>
2023-05-02 10:10:46 +0200

    Implements navigation controller in favorite pokemon

--- FavoritePokemon+CoreDataClass.swift
+++ FavoritePokemon+CoreDataClass.swift
@@ -11,5 +11,12 @@ import CoreData
 
 @objc(FavoritePokemon)
 public class FavoritePokemon: NSManagedObject {
-
+  // MARK: - Constants
+/// Pokemon u Core Dati bi trebao sadrzavati varijable koje imam u detaljnom pregledu, ID Pokemona, mozda neku manju slikicu, koja ce mi biti u detaljnom pregledu.
+  var pokemonIDCoreData: Int?
+  var pokemonNameCoreData: String?
+  var spriteURLCoreData: URL?
+  var pokemonWeightCoreData: Int?
+  var pokemonExpCoreData: Int?
+  var isFavoriteCoreData: Bool?
 }
